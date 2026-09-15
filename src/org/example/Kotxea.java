6c811cd4f4c40a683b718f476bb72d31297cf1af
Aimar Mayo <aimarmayokorta@gmail.com>
2026-09-15 12:25:52 +0200

    Konstanteak

--- src/org/example/Kotxea.java
+++ src/org/example/Kotxea.java
@@ -1,6 +1,8 @@
 package org.example;
 
 public class Kotxea extends Ibilgailua{
+    final static int GURPIL_KOPURUA = 4;
+
     public Kotxea(String marka, String modeloa, int urtea) {
         super(marka, modeloa, urtea);
     }
